4e24e503d8399f50e2fefb15b53c09cd3fb8b0b7
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-10-14 09:54:44 +0800

    Install Tailwind

--- docs/src/Page.tsx
+++ docs/src/Page.tsx
@@ -7,6 +7,10 @@ type Props = {
 };
 
 const Page: FC<Props> = ({ title, description, children }) => {
+  const pageContent = (
+    <div class="mx-auto min-h-screen max-w-6xl">{children}</div>
+  );
+
   return html`<!DOCTYPE html>
     <html lang="en">
       <head>
@@ -14,10 +18,11 @@ const Page: FC<Props> = ({ title, description, children }) => {
         <meta name="description" content="${description}" />
         <meta name="color-scheme" content="light dark" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href="/styles.css" rel="stylesheet" />
         <title>${title}</title>
       </head>
       <body>
-        ${children}
+        ${pageContent}
       </body>
     </html>`;
 };
---
+++ docs/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // TODO: Refactor code and change entrypoints accordingly
+  content: ["./{scripts,src,utils}/**/*.{html,js,jsx,ts,tsx}", "./index.ts"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
